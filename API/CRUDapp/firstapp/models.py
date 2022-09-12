56cae44c9702ea1021b33a8270b54da68e7df6c2
Garrettgar21 <garrettgar21@gmail.com>
2022-09-12 00:47:38 -0400

    Adding in local db
    I had trouble connecting to an Azure db, so I created one locally

--- API/CRUDapp/firstapp/models.py
+++ API/CRUDapp/firstapp/models.py
@@ -1,3 +1,14 @@
+from operator import mod
 from django.db import models
 
 # Create your models here.
+
+class ImgurModel(models.Model):
+    ImageId = models.AutoField(primary_key=True)
+    ImageName= models.CharField(max_length=50)
+    ImageDescription = models.CharField(max_length=50)
+
+class User(models.Model):
+    UserId = models.AutoField
+    UserUsername = models.CharField(max_length=50)
+    UserPassword = models.CharField(max_length=50)
