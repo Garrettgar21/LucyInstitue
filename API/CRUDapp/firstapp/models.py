cbfdf3c31819e536ceaff6ba32cdde4edbcb27d8
Garrettgar21 <56242627+Garrettgar21@users.noreply.github.com>
2022-09-12 21:10:56 -0400

    Revert "Another branch"

--- API/CRUDapp/firstapp/models.py
+++ API/CRUDapp/firstapp/models.py
@@ -1,15 +1,14 @@
+from operator import mod
 from django.db import models
 
 # Create your models here.
 
 class ImgurModel(models.Model):
     ImageId = models.AutoField(primary_key=True)
-    ImageName = models.CharField(max_length=100)
-    ImageDescription = models.CharField(max_length=100)
-    ImageURL = models.CharField(max_length=500)
-    ImageFav = models.BooleanField(default=False)
+    ImageName= models.CharField(max_length=50)
+    ImageDescription = models.CharField(max_length=50)
 
 class User(models.Model):
     UserId = models.AutoField
-    UserUsername = models.CharField(max_length=100)
-    UserPassword = models.CharField(max_length=100)
+    UserUsername = models.CharField(max_length=50)
+    UserPassword = models.CharField(max_length=50)
